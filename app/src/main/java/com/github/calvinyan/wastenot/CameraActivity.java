dc7cfbdff34b28546a3f161bf311132bb06d8abd
CalvinYan <calviny524@gmail.com>
2018-10-06 12:45:51 -0700

    Working portrait camera feed on main activity

--- app/src/main/java/com/github/calvinyan/wastenot/CameraActivity.java
+++ app/src/main/java/com/github/calvinyan/wastenot/CameraActivity.java
@@ -1,13 +1,42 @@
 package com.github.calvinyan.wastenot;
 
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.FrameLayout;
 
 public class CameraActivity extends AppCompatActivity {
+
+    private static String TAG = "CameraActivity";
+
+    private Camera camera;
+    private CameraView cameraView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+
+        try {
+            camera = Camera.open();
+        } catch (Exception e) {
+            Log.e(TAG, "Exception when attempting to access camera!");
+        }
+
+        cameraView = new CameraView(this, camera);
+
+        ((FrameLayout) findViewById(R.id.camera_frame)).addView(cameraView);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (camera != null) {
+            camera.release();
+            camera = null;
+        }
     }
 }
